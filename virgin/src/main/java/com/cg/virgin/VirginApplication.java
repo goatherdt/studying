045c39a2bf09df59c37300d777caaac2605b0fd9
goatherdt <chenggen999@gmail.com>
2022-03-06 13:08:07 +0800

    first project

---
+++ virgin/src/main/java/com/cg/virgin/VirginApplication.java
@@ -0,0 +1,13 @@
+package com.cg.virgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirginApplication.class, args);
+	}
+
+}
---
+++ virgin/src/test/java/com/cg/virgin/VirginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.virgin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
