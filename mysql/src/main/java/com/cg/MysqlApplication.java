9b27af63c7e8d6c053e65046e0584cbc58112c4c
goatherdt <chenggen999@gmail.com>
2022-03-12 15:16:18 +0800

    unit tst

--- mysql/src/main/java/com/cg/MysqlApplication.java
+++ mysql/src/main/java/com/cg/MysqlApplication.java
@@ -1,10 +1,12 @@
 package com.cg;
 
+import com.cg.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MysqlApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlApplication.class, args);
--- mysql/src/test/java/com/cg/mysql/MysqlApplicationTests.java
+++ mysql/src/test/java/com/cg/mysql/MysqlApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cg.mysql;
 
+import com.cg.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class MysqlApplicationTests {
 
+	@Resource
+	private UserRepository userRepository;
 	@Test
 	void contextLoads() {
+		Iterable t = userRepository.findAll();
+		System.out.println(t);
 	}
 
 }
