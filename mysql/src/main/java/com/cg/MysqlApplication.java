3064bbaaa89cbf76033b3ec92b95c6e98a0775cb
goatherdt <chenggen999@gmail.com>
2022-03-13 00:22:06 +0800

    tset

--- mysql/src/main/java/com/cg/MysqlApplication.java
+++ mysql/src/main/java/com/cg/MysqlApplication.java
@@ -3,6 +3,8 @@ package com.cg;
 import com.cg.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class MysqlApplication {
@@ -10,6 +12,7 @@ public class MysqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlApplication.class, args);
+
 	}
 
 }
