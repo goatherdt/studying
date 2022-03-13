e8044588287afafadb41cd78ea29605a8eaed59d
goatherdt <chenggen999@gmail.com>
2022-03-13 13:34:14 +0800

    use annotation ConfigurationProperties

--- mysql/src/main/java/com/cg/MysqlApplication.java
+++ mysql/src/main/java/com/cg/MysqlApplication.java
@@ -1,12 +1,15 @@
 package com.cg;
 
+import com.cg.entity.UserProperty;
 import com.cg.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UserProperty.class)
 public class MysqlApplication {
 
 
---
+++ mysql/src/main/java/com/cg/entity/UserProperty.java
@@ -0,0 +1,7 @@
+package com.cg.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spring.user")
+public class UserProperty {
+}
